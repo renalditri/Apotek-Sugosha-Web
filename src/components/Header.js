2fa77056bc983289f2ab3c5bcf2ef42cff5cdec7
yana sugosha <yana.sugosha@gmail.com>
2021-05-03 21:41:19 +0800

    edit css var and add css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Container, Row, Col, Button } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import cart from '../assets/img/Cart.png';
-import logo from '../assets/img/logo.png';
+import React, { Component } from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Container,
+  Row,
+  Col,
+  Button,
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import cart from "../assets/img/Cart.png";
+import logo from "../assets/img/logo.png";
 
 export default class Header extends Component {
   render() {
@@ -12,36 +22,61 @@ export default class Header extends Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Row className="w-100">
             <Col>
-              <LinkContainer to="/"><Navbar.Brand><img src={logo} /></Navbar.Brand></LinkContainer>
+              <LinkContainer to="/">
+                <Navbar.Brand>
+                  <img src={logo} />
+                </Navbar.Brand>
+              </LinkContainer>
             </Col>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="w-100">
                 <Col>
-                  <LinkContainer to="/kategori"><Nav.Link>Kategori</Nav.Link></LinkContainer>
+                  <LinkContainer to="/kategori">
+                    <Nav.Link>Kategori</Nav.Link>
+                  </LinkContainer>
                 </Col>
                 <Col>
-                  <LinkContainer to="/resep"><Nav.Link>Tebus Resep</Nav.Link></LinkContainer>
+                  <LinkContainer to="/resep">
+                    <Nav.Link>Tebus Resep</Nav.Link>
+                  </LinkContainer>
                 </Col>
                 <Col md={6}>
                   <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2 w-100" />
+                    <FormControl
+                      type="text"
+                      placeholder="Search"
+                      className="mr-sm-2 w-100"
+                    />
                   </Form>
                 </Col>
               </Nav>
             </Navbar.Collapse>
             <Col>
-              <LinkContainer to="/cart"><Nav.Link><img src={cart} width="24" height="24" /></Nav.Link></LinkContainer>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <img src={cart} width="24" height="24" />
+                </Nav.Link>
+              </LinkContainer>
             </Col>
             <Col>
               <NavDropdown title="Hi, nama orang" id="basic-nav-dropdown">
-                <LinkContainer className="small-label" to="/status"><NavDropdown.Item>Status Transaksi</NavDropdown.Item></LinkContainer>
-                <LinkContainer className="small-label" to="/profil"><NavDropdown.Item>Profil</NavDropdown.Item></LinkContainer>
-                <LinkContainer className="small-label" to="/logout"><NavDropdown.Item>Log Out</NavDropdown.Item></LinkContainer>
+                <LinkContainer className="small-label" to="/status">
+                  <NavDropdown.Item>Status Transaksi</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer className="small-label" to="/profil">
+                  <NavDropdown.Item>Profil</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer
+                  className="small-label dropdown-logout"
+                  to="/logout"
+                >
+                  <NavDropdown.Item>Log Out</NavDropdown.Item>
+                </LinkContainer>
               </NavDropdown>
             </Col>
           </Row>
         </Container>
       </Navbar>
-    )
+    );
   }
 }
