da6c576566b4d11db927ba9939b60d982caa770a
Minka <renalditriananda@gmail.com>
2022-02-24 17:38:43 +0700

    heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/main.scss';
+import './css/main.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
